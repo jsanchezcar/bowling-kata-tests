ae8d80170e86aa78d88c6b35e0a61038e47d3c33
Jonatan <jsanchezcar@edu.tecnocampus.cat>
2018-05-04 17:15:22 +0200

    Second test updated, passed, now its easier to read

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -10,18 +10,21 @@ public class BowlingGameTest extends TestCase{
         g = new Game();
     }
 
+    private void rollMany(int n, int pins){
+        for (int i=0; i<n; i++){
+            g.roll(pins);
+        }
+    }
+
     @Test
     public void testGutterGame() throws Exception{
-        for (int i=0; i<20; i++){
-            g.roll(0);
-        }
+        rollMany(20, 0);
         assertEquals(0, g.score());
     }
 
     @Test
     public void testAllOnes() throws Exception {
-        for (int i = 0; i < 20; i++)
-            g.roll(1);
+        rollMany(20,1);
         assertEquals(20, g.score());
     }
 }
